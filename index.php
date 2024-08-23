3c4dc26954186a904c2264dd62f40f3fc6c6cd7d
mukhandia <felixmukhandia@gmail.com>
2024-08-23 15:45:59 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,147 @@
+<?php
+session_start();
+include "includes/header.php";
+
+?>
+
+<div class="page-content-wrapper ">
+
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="page-title-box">
+                    <div class="btn-group float-right">
+                        <ol class="breadcrumb hide-phone p-0 m-0">
+                            <li class="breadcrumb-item"><a href="#">Afripixel</a></li>
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                    </div>
+                    <h4 class="page-title">Dashboard</h4>
+                </div>
+            </div>
+        </div>
+        <!-- end page title end breadcrumb -->
+        <div class="row">
+            <div class="col-lg-9">
+                <div class="row">
+                    <div class="col-lg-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="icon-contain">
+                                    <div class="row">
+                                        <div class="col-2 align-self-center">
+                                            <i class="fas fa-tasks text-gradient-success"></i>
+                                        </div>
+                                        <div class="col-10 text-right">
+                                            <h5 class="mt-0 mb-1">190</h5>
+                                            <p class="mb-0 font-12 text-muted">Active Tasks</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="card">
+                            <div class="card-body justify-content-center">
+                                <div class="icon-contain">
+                                    <div class="row">
+                                        <div class="col-2 align-self-center">
+                                            <i class="far fa-gem text-gradient-danger"></i>
+                                        </div>
+                                        <div class="col-10 text-right">
+                                            <h5 class="mt-0 mb-1">62</h5>
+                                            <p class="mb-0 font-12 text-muted">Project</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="icon-contain">
+                                    <div class="row">
+                                        <div class="col-2 align-self-center">
+                                            <i class="fas fa-users text-gradient-warning"></i>
+                                        </div>
+                                        <div class="col-10 text-right">
+                                            <h5 class="mt-0 mb-1">14</h5>
+                                            <p class="mb-0 font-12 text-muted">Teams</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-lg-3">
+                        <div class="card ">
+                            <div class="card-body">
+                                <div class="icon-contain">
+                                    <div class="row">
+                                        <div class="col-2 align-self-center">
+                                            <i class="fas fa-database text-gradient-primary"></i>
+                                        </div>
+                                        <div class="col-10 text-right">
+                                            <h5 class="mt-0 mb-1">$15562</h5>
+                                            <p class="mb-0 font-12 text-muted">Budget</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="header-title pb-3 mt-0">Payments</h5>
+                        <div class="table-responsive">
+                            <table class="table table-hover mb-0">
+                                <thead>
+                                    <tr class="align-self-center">
+                                        <th>Project Name</th>
+                                        <th>Client Name</th>
+                                        <th>Payment Type</th>
+                                        <th>Paid Date</th>
+                                        <th>Amount</th>
+                                        <th>Transaction</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Product Devlopment</td>
+                                        <td>
+                                            <img src="assets/images/users/avatar-1.jpg" alt="" class="thumb-sm rounded-circle mr-2">
+                                            Kevin Heal
+                                        </td>
+                                        <td>Paypal</td>
+                                        <td>5/8/2018</td>
+                                        <td>$15,000</td>
+                                        <td><span class="badge badge-boxed  badge-soft-warning">panding</span></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div><!--end table-responsive-->
+                        <div class="pt-3 border-top text-right">
+                            <a href="#" class="text-primary">View all <i class="mdi mdi-arrow-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end row -->
+
+    </div><!-- container -->
+
+</div> <!-- Page content Wrapper -->
+
+<?php
+include "includes/footer.php";
+?>
